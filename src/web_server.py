ce4755cd1477a123462ed5e7bf7af15e5f0818fd
Zachary Kohnen <me@dusterthefirst.com>
2022-10-20 13:45:37 +0200

    Appease CORS

--- src/web_server.py
+++ src/web_server.py
@@ -19,25 +19,30 @@ async def serve_client(reader: asyncio.StreamReader, writer: asyncio.StreamWrite
     print(reader.get_extra_info("peername"), "url", url)
     print(reader.get_extra_info("peername"), "http", http)
 
-    if method == "GET":
+    if method == "GET" or method == "OPTIONS":
         if url == "/api/health":
             writer.write(http_response("text/plain"))
-            writer.write("OK")
+            if method == "GET":
+                writer.write("OK")
         elif url == "/api/events":
             writer.write(http_response("text/event-stream"))
-            await writer.drain()
-            await events.handle_events(writer, channel.create_receiver())
+            if method == "GET":
+                await writer.drain()
+                await events.handle_events(writer, channel.create_receiver())
         elif url == "/api/solenoid/open":
             writer.write(http_response("text/plain"))
-            writer.write("OK")
-            api.solenoid(channel, False)
+            if method == "GET":
+                writer.write("OK")
+                api.solenoid(channel, False)
         elif url == "/api/solenoid/close":
             writer.write(http_response("text/plain"))
-            writer.write("OK")
-            api.solenoid(channel, True)
+            if method == "GET":
+                writer.write("OK")
+                api.solenoid(channel, True)
         else:
             writer.write(http_response("text/plain", status=(404, "Not Found")))
-            writer.write("Not Found")
+            if method == "GET":
+                writer.write("Not Found")
     else:
         writer.write(http_response("text/plain", status=(405, "Method Not Allowed")))
         writer.write("Method Not Allowed")
@@ -46,12 +51,13 @@ async def serve_client(reader: asyncio.StreamReader, writer: asyncio.StreamWrite
     await writer.wait_closed()
     print(reader.get_extra_info("peername"), method, url, "Client disconnected")
 
-def http_response(content_type: str, status: tuple[int, str] = (200, "OK")):
+def http_response(content_type: str | None, status: tuple[int, str] = (200, "OK")):
     headers = [
         ("Content-Type", content_type),
-        ("Access-Control-Allow-Origin", "*")
+        ("Access-Control-Allow-Origin", "*"),
+        ("Access-Control-Allow-Headers", "*"),
     ]
 
-    headers = "\r\n".join(map(lambda header: f"{header[0]}: {header[1]}", headers))
+    headers_str = "\r\n".join(map(lambda header: f"{header[0]}: {header[1]}", headers))
 
-    return f"HTTP/1.0 {status[0]} {status[1]}\r\n{headers}\r\n\r\n"
+    return f"HTTP/1.0 {status[0]} {status[1]}\r\n{headers_str}\r\n\r\n"
